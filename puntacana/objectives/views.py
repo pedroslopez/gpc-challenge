64469da8469cc74cd4423cf8172164fc4d9451dd
Pedro S. Lopez <pedroslopez@me.com>
2021-01-19 02:26:50 -0400

    define basic templates and objective list view

--- puntacana/objectives/views.py
+++ puntacana/objectives/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Objective
+
+
+class ObjectiveListView(ListView):
+    model = Objective
